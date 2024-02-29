71fe38b98a44b04a6c94d84e277ae00fa68823b7
KrushnaVardhanReddy <kpakkiragari@gmail.com>
2024-02-29 07:15:52 -0500

    Remove Duplicates from Sorted Array

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # InterviewPrep
-Interviews Preparation
+Interview Preparation
+
+## 1 - Remove Duplicates from Sorted Array
+Leet Code [Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/).  
+YouTube Solution [Remove Duplicates from Sorted Array](https://www.youtube.com/watch?v=DEJAZBq0FDA).
---
+++ RemoveDuplicatesfromSortedArray/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+	lp := 1 // Left Pointer
+
+	// loop through each item
+	for index := 1; index < len(nums); index++ {
+		// compare current with previous item
+		// if they are not equal move current item to left pointer
+		// increase the left pointer
+		if nums[index] != nums[index-1] {
+			nums[lp] = nums[index]
+			lp += 1
+		}
+	}
+
+	fmt.Println("nums :", nums)
+
+	return lp
+}
+
+func main() {
+	// Test Case1 : []int{1, 1, 2}
+	testCase1 := []int{1, 1, 2}
+	// Test Case2 : []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	testCase2 := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+
+	fmt.Println(removeDuplicates(testCase1))
+	fmt.Println(removeDuplicates(testCase2))
+}
